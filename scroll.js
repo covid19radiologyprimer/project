bd7de2c30a1b6a87ff0d62e85faf9ba8eb775856
Venkata Macha <venkatamacha@gmail.com>
2020-04-07 14:31:53 -0500

    Integration of full dataset into platform

--- scroll.js
+++ scroll.js
@@ -1,8 +1,235 @@
 
 var images1 = [];
-for (i = 42; i >= 1; i--) {
+for (i = 1; i <= 42; i++) {
   images1.push("".concat("axial non-contrast 75330/",String(i), ".jpeg"))
 }
+
+var images2 = [];
+for (i = 1; i <= 42; i++) {
+  images2.push("".concat("axial lung window 75330/",String(i), ".jpeg"))
+}
+
+var images3 = [];
+for (i = 1; i <= 65; i++) {
+  images3.push("".concat("coronal lung window 75330/",String(i), ".jpeg"))
+}
+
+var images4 = [];
+for (i = 1; i <= 99; i++) {
+  images4.push("".concat("axial lung window 74867/",String(i), ".jpeg"))
+}
+
+var images5 = [];
+for (i = 10000; i <= 10099; i++) {
+  images5.push("".concat("axial non-contrast 74867/a",String(i), ".jpeg"))
+}
+
+var images6 = [];
+for (i = 10000; i <= 10094; i++) {
+  images6.push("".concat("coronal lung window 74867/a",String(i), ".jpeg"))
+}
+
+var images7 = [];
+for (i = 10000; i <= 10083; i++) {
+  images7.push("".concat("coronal non-contrast 74867/a",String(i), ".jpeg"))
+}
+
+var images8 = [];
+for (i = 10000; i <= 10092; i++) {
+  images8.push("".concat("axial lung window 75417/a",String(i), ".jpeg"))
+}
+
+var images9 = [];
+for (i = 10000; i <= 10081; i++) {
+  images9.push("".concat("coronal lung window 75417/a",String(i), ".jpeg"))
+}
+
+var images10 = [];
+for (i = 10000; i <= 10092; i++) {
+  images10.push("".concat("axial mediastinal window 75417/a",String(i), ".jpeg"))
+}
+
+var images11 = [];
+for (i = 10000; i <= 10081; i++) {
+  images11.push("".concat("coronal mediastinal window 75417/a",String(i), ".jpeg"))
+}
+
+var images12 = [];
+for (i = 10000; i <= 10109; i++) {
+  images12.push("".concat("axial lung window 74887/a",String(i), ".jpeg"))
+}
+
+var images13 = [];
+for (i = 10000; i <= 10078; i++) {
+  images13.push("".concat("axial non-contrast 74887/a",String(i), ".jpeg"))
+}
+
+var images14 = [];
+for (i = 10000; i <= 10072; i++) {
+  images14.push("".concat("coronal lung window 74887/a",String(i), ".jpeg"))
+}
+
+var images15 = [];
+for (i = 10000; i <= 10065; i++) {
+  images15.push("".concat("axial lung window 75281/a",String(i), ".jpeg"))
+}
+
+var images16 = [];
+for (i = 10000; i <= 10043; i++) {
+  images16.push("".concat("axial non-contrast 75281/a",String(i), ".jpeg"))
+}
+
+var images17 = [];
+for (i = 10000; i <= 10044; i++) {
+  images17.push("".concat("axial non-contrast 75350/a",String(i), ".jpeg"))
+}
+
+var images18 = [];
+for (i = 10000; i <= 10038; i++) {
+  images18.push("".concat("axial non-contrast 74879/a",String(i), ".jpeg"))
+}
+
+var images19 = [];
+for (i = 10000; i <= 10038; i++) {
+  images19.push("".concat("axial lung window 74879/a",String(i), ".jpeg"))
+}
+
+var images20 = [];
+for (i = 10000; i <= 10081; i++) {
+  images20.push("".concat("coronal lung window 74879/a",String(i), ".jpeg"))
+}
+
+var images21 = [];
+for (i = 10000; i <= 10417; i++) {
+  images21.push("".concat("axial lung window 74879 2/a",String(i), ".jpeg"))
+}
+
+var images22 = [];
+for (i = 10000; i <= 10080; i++) {
+  images22.push("".concat("axial lung window 75249/a",String(i), ".jpeg"))
+}
+
+var images23 = [];
+for (i = 10000; i <= 10070; i++) {
+  images23.push("".concat("coronal lung window 75249/a",String(i), ".jpeg"))
+}
+
+var images24 = [];
+for (i = 10000; i <= 10074; i++) {
+  images24.push("".concat("axial non-contrast 74560/a",String(i), ".jpeg"))
+}
+
+var images25 = [];
+for (i = 10000; i <= 10074; i++) {
+  images25.push("".concat("axial lung window 74560/a",String(i), ".jpeg"))
+}
+
+var images26 = [];
+for (i = 10000; i <= 10041; i++) {
+  images26.push("".concat("axial non-contrast 75329/a",String(i), ".jpeg"))
+}
+
+var images27 = [];
+for (i = 10000; i <= 10041; i++) {
+  images27.push("".concat("axial lung window 75329/a",String(i), ".jpeg"))
+}
+
+var images28 = [];
+for (i = 10000; i <= 10041; i++) {
+  images28.push("".concat("axial non-contrast 75329 2/a",String(i), ".jpeg"))
+}
+
+var images29 = [];
+for (i = 10000; i <= 10041; i++) {
+  images29.push("".concat("axial lung window 75329 2/a",String(i), ".jpeg"))
+}
+
+var images30 = [];
+for (i = 10000; i <= 10070; i++) {
+  images30.push("".concat("coronal lung window 75329/a",String(i), ".jpeg"))
+}
+
+var images31 = [];
+for (i = 10000; i <= 10038; i++) {
+  images31.push("".concat("axial non-contrast 74576/a",String(i), ".jpeg"))
+}
+
+var images32 = [];
+for (i = 10000; i <= 10038; i++) {
+  images32.push("".concat("axial lung window 74576/a",String(i), ".jpeg"))
+}
+
+var images33 = [];
+for (i = 10000; i <= 10066; i++) {
+  images33.push("".concat("coronal lung window 74576/a",String(i), ".jpeg"))
+}
+
+var images34 = [];
+for (i = 10000; i <= 10066; i++) {
+  images34.push("".concat("coronal lung window 74576 2/a",String(i), ".jpeg"))
+}
+
+var images35 = [];
+for (i = 10000; i <= 10044; i++) {
+  images35.push("".concat("axial lung window 74724/a",String(i), ".jpeg"))
+}
+
+var images36 = [];
+for (i = 10000; i <= 10055; i++) {
+  images36.push("".concat("axial liver window 74724/a",String(i), ".jpeg"))
+}
+
+var images37 = [];
+for (i = 10000; i <= 10060; i++) {
+  images37.push("".concat("coronal lung window 74724/a",String(i), ".jpeg"))
+}
+
+var images38 = [];
+for (i = 10000; i <= 10032; i++) {
+  images38.push("".concat("coronal c+ arterial phase 73893/a",String(i), ".jpeg"))
+}
+
+var images39 = [];
+for (i = 10000; i <= 10034; i++) {
+  images39.push("".concat("axial non-contrast 74880/a",String(i), ".jpeg"))
+}
+
+var images40 = [];
+for (i = 10000; i <= 10034; i++) {
+  images40.push("".concat("axial lung window 74880/a",String(i), ".jpeg"))
+}
+
+var images41 = [];
+for (i = 10000; i <= 10070; i++) {
+  images41.push("".concat("axial non-contrast 74880 2/a",String(i), ".jpeg"))
+}
+
+var images42 = [];
+for (i = 10000; i <= 10041; i++) {
+  images42.push("".concat("axial lung window 75345/a",String(i), ".jpeg"))
+}
+
+var images43 = [];
+for (i = 10000; i <= 10086; i++) {
+  images43.push("".concat("axial liver window 75345/a",String(i), ".jpeg"))
+}
+
+var images44 = [];
+for (i = 10000; i <= 10067; i++) {
+  images44.push("".concat("coronal lung window 75345/a",String(i), ".jpeg"))
+}
+
+var images45 = [];
+for (i = 10000; i <= 10113; i++) {
+  images45.push("".concat("axial C+ portal 75349/a",String(i), ".jpeg"))
+}
+
+var images46 = [];
+for (i = 10000; i <= 10113; i++) {
+  images46.push("".concat("axial lung window 75349/a",String(i), ".jpeg"))
+}
+
+
 
 images = images1;
 
@@ -19,6 +246,7 @@ function ImageStack (options) {
   self.stack.style.width = options.width;
   self.stack.style.backgroundSize = 'cover'
   self.stack.style.position = 'relative';
+  self.stack.style.margin = 'auto';
   
   var typeRegex = /(\D+)/
   var sizeType = options.height.match(typeRegex)[0]
@@ -88,240 +316,240 @@ var image_height = '630px';
 var image_width = '630px';
 
 var stack1 = new ImageStack({
-  images: images,
+  images: images1,
   height: image_height,
   width: image_width
 });
 var stack2 = new ImageStack({
-  images: images,
+  images: images2,
   height: image_height,
   width: image_width
 });
 var stack3 = new ImageStack({
-  images: images,
-  height: '500px',
-  width: '500px'
+  images: images3,
+  height: image_height,
+  width: image_width
 });
 
 var stack4 = new ImageStack({
-  images: images,
-  height: '500px',
-  width: '500px'
+  images: images4,
+  height: image_height,
+  width: image_width
 });
 var stack5 = new ImageStack({
-  images: images,
-  height: '500px',
-  width: '500px'
+  images: images5,
+  height: image_height,
+  width: image_width
 });
 var stack6 = new ImageStack({
-  images: images,
-  height: '500px',
-  width: '500px'
+  images: images6,
+  height: image_height,
+  width: image_width
 });
 var stack7 = new ImageStack({
-  images: images,
-  height: '500px',
-  width: '500px'
+  images: images7,
+  height: image_height,
+  width: image_width
 });
 
 var stack8 = new ImageStack({
-  images: images,
-  height: '500px',
-  width: '500px'
+  images: images8,
+  height: image_height,
+  width: image_width
 });
 var stack9 = new ImageStack({
-  images: images,
-  height: '500px',
-  width: '500px'
+  images: images9,
+  height: image_height,
+  width: image_width
 });
 var stack10 = new ImageStack({
-  images: images,
-  height: '500px',
-  width: '500px'
+  images: images10,
+  height: image_height,
+  width: image_width
 });
 var stack11 = new ImageStack({
-  images: images,
-  height: '500px',
-  width: '500px'
+  images: images11,
+  height: image_height,
+  width: image_width
 });
 
 var stack12 = new ImageStack({
-  images: images,
-  height: '500px',
-  width: '500px'
+  images: images12,
+  height: image_height,
+  width: image_width
 });
 var stack13 = new ImageStack({
-  images: images,
-  height: '500px',
-  width: '500px'
+  images: images13,
+  height: image_height,
+  width: image_width
 });
 var stack14 = new ImageStack({
-  images: images,
-  height: '500px',
-  width: '500px'
+  images: images14,
+  height: image_height,
+  width: image_width
 });
 var stack15 = new ImageStack({
-  images: images,
-  height: '500px',
-  width: '500px'
+  images: images15,
+  height: image_height,
+  width: image_width
 });
 
 var stack16 = new ImageStack({
-  images: images,
-  height: '500px',
-  width: '500px'
+  images: images16,
+  height: image_height,
+  width: image_width
 });
 var stack17 = new ImageStack({
-  images: images,
-  height: '500px',
-  width: '500px'
+  images: images17,
+  height: image_height,
+  width: image_width
 });
 var stack18 = new ImageStack({
-  images: images,
-  height: '500px',
-  width: '500px'
+  images: images18,
+  height: image_height,
+  width: image_width
 });
 var stack19 = new ImageStack({
-  images: images,
-  height: '500px',
-  width: '500px'
+  images: images19,
+  height: image_height,
+  width: image_width
 });
 
 var stack20 = new ImageStack({
-  images: images,
-  height: '500px',
-  width: '500px'
+  images: images20,
+  height: image_height,
+  width: image_width
 });
 var stack21 = new ImageStack({
-  images: images,
-  height: '500px',
-  width: '500px'
+  images: images21,
+  height: image_height,
+  width: image_width
 });
 var stack22 = new ImageStack({
-  images: images,
-  height: '500px',
-  width: '500px'
+  images: images22,
+  height: image_height,
+  width: image_width
 });
 var stack23 = new ImageStack({
-  images: images,
-  height: '500px',
-  width: '500px'
+  images: images23,
+  height: image_height,
+  width: image_width
 });
 
 var stack24 = new ImageStack({
-  images: images,
-  height: '500px',
-  width: '500px'
+  images: images24,
+  height: image_height,
+  width: image_width
 });
 var stack25 = new ImageStack({
-  images: images,
-  height: '500px',
-  width: '500px'
+  images: images25,
+  height: image_height,
+  width: image_width
 });
 var stack26 = new ImageStack({
-  images: images,
-  height: '500px',
-  width: '500px'
+  images: images26,
+  height: image_height,
+  width: image_width
 });
 var stack27 = new ImageStack({
-  images: images,
-  height: '500px',
-  width: '500px'
+  images: images27,
+  height: image_height,
+  width: image_width
 });
 var stack28 = new ImageStack({
-  images: images,
-  height: '500px',
-  width: '500px'
+  images: images28,
+  height: image_height,
+  width: image_width
 });
 var stack29 = new ImageStack({
-  images: images,
-  height: '500px',
-  width: '500px'
+  images: images29,
+  height: image_height,
+  width: image_width
 });
 var stack30 = new ImageStack({
-  images: images,
-  height: '500px',
-  width: '500px'
+  images: images30,
+  height: image_height,
+  width: image_width
 });
 var stack31 = new ImageStack({
-  images: images,
-  height: '500px',
-  width: '500px'
+  images: images31,
+  height: image_height,
+  width: image_width
 });
 var stack32 = new ImageStack({
-  images: images,
-  height: '500px',
-  width: '500px'
+  images: images32,
+  height: image_height,
+  width: image_width
 });
 var stack33 = new ImageStack({
-  images: images,
-  height: '500px',
-  width: '500px'
+  images: images33,
+  height: image_height,
+  width: image_width
 });
 var stack34 = new ImageStack({
-  images: images,
-  height: '500px',
-  width: '500px'
+  images: images34,
+  height: image_height,
+  width: image_width
 });
 var stack35 = new ImageStack({
-  images: images,
-  height: '500px',
-  width: '500px'
+  images: images35,
+  height: image_height,
+  width: image_width
 });
 var stack36 = new ImageStack({
-  images: images,
-  height: '500px',
-  width: '500px'
+  images: images36,
+  height: image_height,
+  width: image_width
 });
 var stack37 = new ImageStack({
-  images: images,
-  height: '500px',
-  width: '500px'
+  images: images37,
+  height: image_height,
+  width: image_width
 });
 var stack38 = new ImageStack({
-  images: images,
-  height: '500px',
-  width: '500px'
+  images: images38,
+  height: image_height,
+  width: image_width
 });
 var stack39 = new ImageStack({
-  images: images,
-  height: '500px',
-  width: '500px'
+  images: images39,
+  height: image_height,
+  width: image_width
 });
 var stack40 = new ImageStack({
-  images: images,
-  height: '500px',
-  width: '500px'
+  images: images40,
+  height: image_height,
+  width: image_width
 });
 var stack41 = new ImageStack({
-  images: images,
-  height: '500px',
-  width: '500px'
+  images: images41,
+  height: image_height,
+  width: image_width
 });
 var stack42 = new ImageStack({
-  images: images,
-  height: '500px',
-  width: '500px'
+  images: images42,
+  height: image_height,
+  width: image_width
 });
 var stack43 = new ImageStack({
-  images: images,
-  height: '500px',
-  width: '500px'
+  images: images43,
+  height: image_height,
+  width: image_width
 });
 var stack44 = new ImageStack({
-  images: images,
-  height: '500px',
-  width: '500px'
+  images: images44,
+  height: image_height,
+  width: image_width
 });
 var stack45 = new ImageStack({
-  images: images,
-  height: '500px',
-  width: '500px'
+  images: images45,
+  height: image_height,
+  width: image_width
 });
 var stack46 = new ImageStack({
-  images: images,
-  height: '500px',
-  width: '500px'
+  images: images46,
+  height: image_height,
+  width: image_width
 });
 
 document.querySelector('.stack1').appendChild(stack1);
